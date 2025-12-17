e901cf8ced18cf84e2783f1f453f6d1a921b9347
Emily <emilycxt2015@gmail.com>
2025-12-17 15:04:49 -0300

    App init

---
+++ src/main/java/com/dev/emilycxt/app/ZeloProApplication.java
@@ -0,0 +1,14 @@
+package com.dev.emilycxt.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ZeloProApplication extends Application{
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
